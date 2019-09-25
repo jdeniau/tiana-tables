2b48283973fc696ad5b73af22e07864d180708bf
Julien Deniau <julien.deniau@gmail.com>
2019-09-25 23:22:25 +0200

    try to add connection in storybook

--- stories/1-Connection.stories.tsx
+++ stories/1-Connection.stories.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import Connection from '../src/component/Connection/ConnectionForm';
+import { MemoryRouter as Router } from 'react-router';
+import ConnectionForm from '../src/component/Connection/ConnectionForm';
+import ConnectionNav from '../src/component/Connection/Nav';
 import { ConnectionContext } from '../src/Contexts';
+import { createConnection } from 'mysql';
 
 const stories = storiesOf('Connection Block', module);
 
@@ -22,7 +25,30 @@ stories.add('Connection Block', () => {
         setCurrentConnection: action('setCurrentConnection'),
       }}
     >
-      <Connection />
+      <ConnectionForm />
     </ConnectionContext.Provider>
+  );
+});
+
+stories.add('Connection nav', () => {
+  const stubbedConnection = createConnection({
+    host: 'example.org',
+    user: 'bob',
+    password: 'secret',
+  });
+
+  return (
+    <Router>
+      <ConnectionContext.Provider
+        value={{
+          currentConnection: null,
+          connectionList: [stubbedConnection],
+          connectTo: action('connectTo'),
+          setCurrentConnection: action('setCurrentConnection'),
+        }}
+      >
+        <ConnectionNav />
+      </ConnectionContext.Provider>
+    </Router>
   );
 });
