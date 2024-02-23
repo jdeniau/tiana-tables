03fe022e9abd009f2a30f349302e3ee179ffee08
Julien Deniau <julien.deniau@gmail.com>
2024-02-23 23:01:51 +0100

    set default theme

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -25,7 +25,7 @@ const preview: Preview = {
   globalTypes: {
     theme: {
       description: 'Global theme for components',
-      defaultValue: 'Visual Studio', //DEFAULT_THEME.name,
+      defaultValue: DEFAULT_THEME.name,
       toolbar: {
         // The label to show for this toolbar item
         title: 'Theme',
