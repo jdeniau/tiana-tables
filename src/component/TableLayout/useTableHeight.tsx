49d87986908addb639bf7dd98a685ef1728bcdd7
Julien Deniau <julien.deniau@gmail.com>
2024-03-04 15:16:37 +0100

    Split "TableLayout" in multiple files

---
+++ src/component/TableLayout/useTableHeight.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useMemo, useRef, useState } from 'react';
+
+export function useTableHeight(): [number, React.RefObject<HTMLDivElement>] {
+  const [yTableScroll, setYTableScroll] = useState<number>(0);
+  const resizeRef = useRef<HTMLDivElement | null>(null);
+
+  const resizeObserver = useMemo(
+    () =>
+      new ResizeObserver((entries) => {
+        const entry = entries[0];
+        const divHeight = entry.borderBoxSize[0].blockSize;
+
+        const tableHeader =
+          resizeRef.current?.querySelector('.ant-table-header');
+
+        const tableHeaderHeight =
+          tableHeader?.getBoundingClientRect().height ?? 0;
+
+        // I don't know why we need to subtract 1 from the height, but if not, the div will have a scrollbar
+        setYTableScroll(divHeight - tableHeaderHeight - 1);
+      }),
+    []
+  );
+
+  useEffect(() => {
+    const resizeRefCurrent = resizeRef.current;
+
+    if (resizeRefCurrent) {
+      resizeObserver.observe(resizeRefCurrent);
+    }
+
+    return () => {
+      if (resizeRefCurrent) {
+        resizeObserver.unobserve(resizeRefCurrent);
+      }
+    };
+  }, [resizeObserver]);
+
+  return [yTableScroll, resizeRef];
+}
