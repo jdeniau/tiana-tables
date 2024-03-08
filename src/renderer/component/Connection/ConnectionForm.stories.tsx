3a60538ba99027b37bb636b6214ae031eafc6cb7
Julien Deniau <julien.deniau@gmail.com>
2024-03-08 23:40:49 +0100

    add interaction tests on ConnectionForm story

--- src/renderer/component/Connection/ConnectionForm.stories.tsx
+++ src/renderer/component/Connection/ConnectionForm.stories.tsx
@@ -1,23 +1,38 @@
 import { action } from '@storybook/addon-actions';
 import type { Meta, StoryObj } from '@storybook/react';
-import { ConnectionContext } from '../../../contexts/ConnectionContext';
+import { expect, fn, userEvent, within } from '@storybook/test';
+import { testables } from '../../../contexts/ConfigurationContext';
 import ConnectionForm from './ConnectionForm';
+
+const { ConfigurationContext } = testables;
+
+const addConnectionToConfig = fn();
 
 const meta: Meta<typeof ConnectionForm> = {
   component: ConnectionForm,
   decorators: [
-    (Story) => (
-      <ConnectionContext.Provider
+    (Story, { globals: { theme } }) => (
+      <ConfigurationContext.Provider
         value={{
-          currentConnectionName: 'test',
-          connectionNameList: ['test'],
-          addConnectionToList: async (connectionName) => {
-            action('addConnectionToList')(connectionName);
+          configuration: {
+            version: 1,
+            theme,
+            connections: {},
+          },
+          addConnectionToConfig: (connection) => {
+            action('addConnectionToConfig')(connection);
+            addConnectionToConfig(connection);
+          },
+          updateConnectionState: (connectionName, key, value) => {
+            action('updateConnectionState')(connectionName, key, value);
+          },
+          editConnection: (name, connection) => {
+            action('editConnection')(name, connection);
           },
         }}
       >
         <Story />
-      </ConnectionContext.Provider>
+      </ConfigurationContext.Provider>
     ),
   ],
 };
@@ -30,7 +45,36 @@ type Story = StoryObj<typeof ConnectionForm>;
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
-export const Create: Story = {};
+export const Create: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.type(
+      canvas.getByRole('textbox', {
+        name: /name/i,
+      }),
+      'some personnal name'
+    );
+
+    const host = canvas.getByRole('textbox', {
+      name: /host/i,
+    });
+
+    await userEvent.clear(host);
+
+    await userEvent.type(host, 'my.database.com');
+
+    await userEvent.click(canvas.getByText(/save and connect/i));
+
+    expect(addConnectionToConfig).toHaveBeenCalledWith({
+      name: 'some personnal name',
+      host: 'my.database.com',
+      port: 3306,
+      user: 'root',
+      password: '',
+    });
+  },
+};
 
 export const Edit: Story = {
   args: {
