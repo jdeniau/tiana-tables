a9064a701bd8f4681a9e4310562d154716daf250
Julien Deniau <julien.deniau@gmail.com>
2024-04-03 09:36:10 +0200

    add tests for completion

---
+++ src/renderer/component/MonacoEditor/useCompletion.test.ts
@@ -0,0 +1,83 @@
+/**
+ * @vitest-environment jsdom
+ */
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import { describe, expect, test, vi } from 'vitest';
+import { ShowTableStatus } from '../../../sql/types';
+import { testables } from './useCompletion';
+
+const { provideCompletionItems, SQL_KEYWORDS } = testables;
+
+describe('provideCompletionItems', () => {
+  test.each([
+    { sql: '' },
+    { sql: 'SELECT * ' },
+    { sql: 'SELECT * FROM table_name ' },
+    { sql: 'SELECT * FROM table_name ', column: 1 },
+    { sql: 'SELECT * FROM table_name WHERE foo = bar ' },
+  ])('should return suggestions for SQL keywords', ({ sql, column }) => {
+    const definedColumn = column ?? sql.length + 1;
+    const tableList: ShowTableStatus[] = [];
+    const position = new monaco.Position(1, definedColumn);
+
+    const model = monaco.editor.createModel(sql, 'sql');
+
+    const result = provideCompletionItems(monaco.languages, tableList)(
+      model,
+      position,
+      { triggerKind: monaco.languages.CompletionTriggerKind.Invoke },
+      { isCancellationRequested: false, onCancellationRequested: vi.fn() }
+    );
+
+    expect(result).toEqual({
+      suggestions: SQL_KEYWORDS.map((keyword) => ({
+        label: keyword,
+        kind: monaco.languages.CompletionItemKind.Keyword,
+        insertText: keyword,
+        range: new monaco.Range(1, definedColumn - 1, 1, definedColumn),
+      })),
+    });
+  });
+
+  test.each([
+    { sql: 'SELECT * FROM' },
+    { sql: 'SELECT * FROM ' },
+    { sql: 'SELECT * FROM database.' },
+    { sql: 'SELECT * FROM WHERE foo = bar ', column: 15 },
+    { sql: 'SELECT * FROM table_name WHERE foo = bar ', column: 15 },
+    { sql: 'SELECT * FROM table_name JOIN ' },
+    { sql: 'SELECT * FROM table_name LEFT JOIN ' },
+    { sql: 'SELECT * FROM table_name LEFT JOIN' },
+  ])(
+    'should return tablelist if we are after FROM or JOIN',
+    ({ sql, column }) => {
+      const definedColumn = column ?? sql.length + 1;
+      const tableList: ShowTableStatus[] = [
+        // @ts-expect-error don't want all data, only the name
+        { Name: 'table1' },
+        // @ts-expect-error don't want all data, only the name
+        { Name: 'table2' },
+      ];
+
+      const position = new monaco.Position(1, definedColumn);
+
+      const model = monaco.editor.createModel(sql, 'sql');
+
+      const result = provideCompletionItems(monaco.languages, tableList)(
+        model,
+        position,
+        { triggerKind: monaco.languages.CompletionTriggerKind.Invoke },
+        { isCancellationRequested: false, onCancellationRequested: vi.fn() }
+      );
+
+      expect(result).toEqual({
+        suggestions: tableList.map((table) => ({
+          label: table.Name,
+          kind: monaco.languages.CompletionItemKind.Variable,
+          insertText: table.Name,
+          range: new monaco.Range(1, definedColumn, 1, definedColumn),
+        })),
+      });
+    }
+  );
+});
--- src/renderer/component/MonacoEditor/useCompletion.tsx
+++ src/renderer/component/MonacoEditor/useCompletion.tsx
@@ -1,6 +1,14 @@
 import { useEffect } from 'react';
+import {
+  CancellationToken,
+  Position,
+  Range,
+  editor,
+  languages,
+} from 'monaco-editor/esm/vs/editor/editor.api';
 import invariant from 'tiny-invariant';
 import { useTableListContext } from '../../../contexts/TableListContext';
+import { ShowTableStatus } from '../../../sql/types';
 
 const SQL_KEYWORDS = [
   'SELECT',
@@ -15,6 +23,83 @@ const SQL_KEYWORDS = [
   'ON',
 ];
 
+function provideCompletionItems(
+  languages: typeof import('monaco-editor/esm/vs/editor/editor.api').languages,
+  tableList: ShowTableStatus[]
+): languages.CompletionItemProvider['provideCompletionItems'] {
+  return (
+    model: editor.ITextModel,
+    position: Position,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _context: languages.CompletionContext,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _token: CancellationToken
+  ) => {
+    // console.log(model, position);
+    // const textUntilPosition = model.getValueInRange({
+    //   startLineNumber: 1,
+    //   startColumn: 1,
+    //   endLineNumber: position.lineNumber,
+    //   endColumn: position.column,
+    // });
+    // const hasFromBefore = textUntilPosition.match(/FROM\s*$/i);
+    const isAfterFromOrJoin = model.findMatches(
+      '(from|join)\\s*(?<database>\\w*\\.)?(?<tablename>\\w+)?$', // searchString
+      {
+        startLineNumber: 1,
+        startColumn: 1,
+        endLineNumber: position.lineNumber,
+        endColumn: position.column,
+      }, // searchOnlyEditableRange
+      true, // isRegex
+      false, // matchCase
+      null, // wordSeparators
+      true, // captureMatches
+      1 // limitResultCount
+    )?.[0];
+
+    if (isAfterFromOrJoin) {
+      const { matches, range } = isAfterFromOrJoin;
+
+      invariant(matches, 'matches should be defined');
+
+      const fullLength = matches[0]?.length ?? 0;
+      const tableLength = matches[3]?.length ?? 0;
+      const startColumn = range.startColumn + fullLength - tableLength;
+
+      return {
+        suggestions: tableList.map((table) => ({
+          label: table.Name,
+          kind: languages.CompletionItemKind.Variable,
+          insertText: table.Name,
+          range: new Range(
+            range.startLineNumber,
+            startColumn,
+            position.lineNumber,
+            position.column
+          ),
+        })),
+      };
+    }
+
+    const defaultRange = new Range(
+      position.lineNumber,
+      position.column - 1,
+      position.lineNumber,
+      position.column
+    );
+
+    return {
+      suggestions: SQL_KEYWORDS.map((keyword) => ({
+        label: keyword,
+        kind: languages.CompletionItemKind.Keyword,
+        insertText: keyword,
+        range: defaultRange,
+      })),
+    };
+  };
+}
+
 export default function useCompletion(
   monaco: typeof import('monaco-editor/esm/vs/editor/editor.api')
 ) {
@@ -26,70 +111,10 @@ export default function useCompletion(
     // - hover https://microsoft.github.io/monaco-editor/playground.html?source=v0.47.0#example-extending-language-services-hover-provider-example
     const completionItemProvider =
       monaco.languages.registerCompletionItemProvider('sql', {
-        provideCompletionItems: (model, position) => {
-          // console.log(model, position);
-          // const textUntilPosition = model.getValueInRange({
-          //   startLineNumber: 1,
-          //   startColumn: 1,
-          //   endLineNumber: position.lineNumber,
-          //   endColumn: position.column,
-          // });
-          // const hasFromBefore = textUntilPosition.match(/FROM\s*$/i);
-          const isAfterFromOrJoin = model.findMatches(
-            '(from|join)\\s*(?<database>\\w*\\.)?(?<tablename>\\w+)?', // searchString
-            {
-              startLineNumber: 1,
-              startColumn: 1,
-              endLineNumber: position.lineNumber,
-              endColumn: position.column,
-            }, // searchOnlyEditableRange
-            true, // isRegex
-            false, // matchCase
-            null, // wordSeparators
-            true, // captureMatches
-            1 // limitResultCount
-          )?.[0];
-
-          if (isAfterFromOrJoin) {
-            const { matches, range } = isAfterFromOrJoin;
-
-            invariant(matches, 'matches should be defined');
-
-            const fullLength = matches[0]?.length ?? 0;
-            const tableLength = matches[3]?.length ?? 0;
-            const startColumn = range.startColumn + fullLength - tableLength;
-
-            return {
-              suggestions: tableList.map((table) => ({
-                label: table.Name,
-                kind: monaco.languages.CompletionItemKind.Variable,
-                insertText: table.Name,
-                range: new monaco.Range(
-                  range.startLineNumber,
-                  startColumn,
-                  position.lineNumber,
-                  position.column
-                ),
-              })),
-            };
-          }
-
-          const defaultRange = new monaco.Range(
-            position.lineNumber,
-            position.column - 1,
-            position.lineNumber,
-            position.column
-          );
-
-          return {
-            suggestions: SQL_KEYWORDS.map((keyword) => ({
-              label: keyword,
-              kind: monaco.languages.CompletionItemKind.Keyword,
-              insertText: keyword,
-              range: defaultRange,
-            })),
-          };
-        },
+        provideCompletionItems: provideCompletionItems(
+          monaco.languages,
+          tableList
+        ),
       });
 
     return () => {
@@ -97,3 +122,5 @@ export default function useCompletion(
     };
   }, [monaco.Range, monaco.languages, tableList]);
 }
+
+export const testables = { provideCompletionItems, SQL_KEYWORDS };
