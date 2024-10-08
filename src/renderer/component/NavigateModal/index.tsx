74505c439163a74c0bdfac5c6c55103ca37eac58
Julien Deniau <julien.deniau@gmail.com>
2024-10-08 10:20:49 +0200

    useMemo for perf

--- src/renderer/component/NavigateModal/index.tsx
+++ src/renderer/component/NavigateModal/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { DatabaseOutlined, TableOutlined } from '@ant-design/icons';
 import { useConnectionContext } from '../../../contexts/ConnectionContext';
 import { useDatabaseContext } from '../../../contexts/DatabaseContext';
@@ -16,20 +17,23 @@ export default function NavigateModalContainer(props: Props): JSX.Element {
   const { database } = useDatabaseContext();
   const databaseList = useDatabaseListContext();
 
-  const navigationItemList: Array<NavigationItem> = [
-    ...tableStatusList.map((table) => ({
-      key: `Table-${table.Name}`,
-      name: table.Name,
-      link: `/connections/${currentConnectionSlug}/${database}/tables/${table.Name}`,
-      Icon: <TableOutlined />,
-    })),
-    ...databaseList.map((showDatabase) => ({
-      key: `Database-${showDatabase.Database}`,
-      name: showDatabase.Database,
-      link: `/connections/${currentConnectionSlug}/${showDatabase.Database}`,
-      Icon: <DatabaseOutlined />,
-    })),
-  ];
+  const navigationItemList: Array<NavigationItem> = useMemo(
+    () => [
+      ...tableStatusList.map((table) => ({
+        key: `Table-${table.Name}`,
+        name: table.Name,
+        link: `/connections/${currentConnectionSlug}/${database}/tables/${table.Name}`,
+        Icon: <TableOutlined />,
+      })),
+      ...databaseList.map((showDatabase) => ({
+        key: `Database-${showDatabase.Database}`,
+        name: showDatabase.Database,
+        link: `/connections/${currentConnectionSlug}/${showDatabase.Database}`,
+        Icon: <DatabaseOutlined />,
+      })),
+    ],
+    [currentConnectionSlug, database, databaseList, tableStatusList]
+  );
 
   return <NavigateModal navigationItemList={navigationItemList} {...props} />;
 }
